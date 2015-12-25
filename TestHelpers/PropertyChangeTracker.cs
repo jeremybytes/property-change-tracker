6505164e85bed6d9a8c475a3b0fa224c3beb55c9
Jeremy Clark <jeremy@jeremybytes.com>
2015-12-25 15:46:35 -0800

    Original Property Change Tracker code

--- TestHelpers/PropertyChangeTracker.cs
+++ TestHelpers/PropertyChangeTracker.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,33 @@ namespace TestHelpers
 {
     public class PropertyChangeTracker
     {
+        private List<string> notifications = new List<string>();
+
+        public PropertyChangeTracker(INotifyPropertyChanged changer)
+        {
+            changer.PropertyChanged += (o, e) => { notifications.Add(e.PropertyName); };
+        }
+
+        public string[] ChangedProperties
+        {
+            get { return notifications.ToArray(); }
+        }
+
+        public bool WaitForChange(string propertyName, int maxWaitSeconds)
+        {
+            var startTime = DateTime.Now;
+            while (!notifications.Contains(propertyName))
+            {
+                var diff = DateTime.Now - startTime;
+                if (diff.TotalSeconds > maxWaitSeconds)
+                    return false;
+            }
+            return true;
+        }
+
+        public void Reset()
+        {
+            notifications.Clear();
+        }
     }
 }
